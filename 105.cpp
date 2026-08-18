820cf63ef77535689f40d32110fd107c8b46fb87
fddc007 <87221204+fddc007@users.noreply.github.com>
2026-08-18 10:35:08 +0800

    LeetCode 105. 从前序与中序遍历序列构造二叉树

---
+++ 105.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* f(vector<int>& preorder,int l,int r, vector<int>& inorder,int left,int right,unordered_map<int,int>& map)
+    {
+        int cur = preorder[l];
+        int site = map[cur];
+        int left_len = site-left;
+        int right_len = right-site;
+        TreeNode* root = new TreeNode(cur);
+        if(left_len>=1)
+        {
+            root->left = f(preorder,l+1,l+left_len,inorder,left,site-1,map);
+        }
+        if(right_len>=1)
+        {
+            root->right = f(preorder,l+left_len+1,r,inorder,site+1,right,map);
+        }
+        return root;
+        
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int len =inorder.size();
+        unordered_map<int,int> map;
+        for(int i= 0;i<len;i++)
+        {
+            map.insert({inorder[i],i});
+        }
+        return f(preorder,0,len-1,inorder,0,len-1,map);
+    }
+};
+
+int main()
+{
+    vector<int> a = {3,9,20,15,7};
+    vector<int> b = {9,3,15,20,7};
+    Solution s;
+    TreeNode* cur = s.buildTree(a,b);
+
+
+    return 0;
+}
