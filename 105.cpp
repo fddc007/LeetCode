bfc4482fa9a5625cafa3e285ff33195821efd9f5
fddc007 <87221204+fddc007@users.noreply.github.com>
2026-08-18 10:42:24 +0800

    LeetCode 105. 从前序与中序遍历序列构造二叉树

--- 105.cpp
+++ 105.cpp
@@ -18,24 +18,27 @@ class Solution {
 public:
     TreeNode* f(vector<int>& preorder,int l,int r, vector<int>& inorder,int left,int right,unordered_map<int,int>& map)
     {
+        if(l>r || left>right)
+        {
+            return nullptr;
+        }
         int cur = preorder[l];
         int site = map[cur];
         int left_len = site-left;
         int right_len = right-site;
         TreeNode* root = new TreeNode(cur);
-        if(left_len>=1)
-        {
-            root->left = f(preorder,l+1,l+left_len,inorder,left,site-1,map);
-        }
-        if(right_len>=1)
-        {
-            root->right = f(preorder,l+left_len+1,r,inorder,site+1,right,map);
-        }
+        root->left = f(preorder,l+1,l+left_len,inorder,left,site-1,map);
+        root->right = f(preorder,l+left_len+1,r,inorder,site+1,right,map);
         return root;
         
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int len =inorder.size();
+        int len_preorder = preorder.size();
+        if(len == 0 || len_preorder == 0 || len != len_preorder)
+        {
+            return nullptr;
+        }
         unordered_map<int,int> map;
         for(int i= 0;i<len;i++)
         {
